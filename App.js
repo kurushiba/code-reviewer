469ed3346e008b7302027de1eee1ab6073e0186b
kurushiba <156669290+kurushiba@users.noreply.github.com>
2024-03-18 14:17:51 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,3 @@
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
-const sample = `
-### 問題点の指摘
-1. \`name\` と \`member1\`, \`member2\`, \`member3\` の比較には、複数の \`||\` 演算子を使っているため、コードが冗長です。
-2. コメントが実際に行われるべき処理や条件の説明を欠いています。
-
-### 問題点を修正し、より簡潔にしたコード
-\`\`\`javascript
-const name = 'yamada';
-const num = 100;
-
-const members = ['sato', 'yamada', 'suzuki'];
-
-if(members.includes(name)){
-    if(num === 0){
-        console.log('0の場合');
-    } else if (num > 0) {
-        console.log('+(プラス)の場合');
-    } else {
-        console.log('- (マイナス)の場合');
-    }
-}
-\`\`\`
-
-### 修正点の説明
-1. \`member1\`, \`member2\`, \`member3\` を配列 \`members\` にまとめました。これにより、複数の要素を効率的かつ読みやすく管理することができます。
-2. \`includes()\` メソッドを使って、指定した要素が配列 \`members\` に含まれるかどうかを確認しました。これにより、1つずつ \`name\` との比較を簡潔に記述できます。
-3. 各条件に対して適切なコメントを追加しました。コメントは、その条件がどのような状況で実行されるかを明確に説明することで、コードの理解を助けます。
-`;
-
 function App() {
   return (
     <div className="min-h-screen bg-blue-100 flex flex-col items-center justify-center">
@@ -47,10 +15,6 @@ function App() {
         </div>
         <div className="flex flex-col w-1/2 h-full items-center justify-center">
           <div className="p-4 overflow-y-auto w-full">
-            <Markdown
-              className="markdown"
-              remarkPlugins={[remarkGfm]}
-            >{ sample }</Markdown>
           </div>
         </div>
       </main>
